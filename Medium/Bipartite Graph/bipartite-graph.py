660d63852a0cba49f4f3fee8bb411eadf542f580
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-03-30 22:30:54 +0530

    Updated Solution - GfG to GitHub

--- Medium/Bipartite Graph/bipartite-graph.py
+++ Medium/Bipartite Graph/bipartite-graph.py
@@ -1,25 +1,36 @@
 from collections import deque
 class Solution:
-    def bfs(self,node,adj,color):
-        q=deque()
-        q.append(node)
-        color[node]=0
-        while q:
-            po=q.popleft()
-            for neighbor in adj[po]:
-                if color[neighbor]==-1:
-                    color[neighbor]=1-color[po]
-                    q.append(neighbor)
-                elif color[neighbor]==color[po]:
+    # def bfs(self,node,adj,color):
+    #     q=deque()
+    #     q.append(node)
+    #     color[node]=0
+    #     while q:
+    #         po=q.popleft()
+    #         for neighbor in adj[po]:
+    #             if color[neighbor]==-1:
+    #                 color[neighbor]=1-color[po]
+    #                 q.append(neighbor)
+    #             elif color[neighbor]==color[po]:
+    #                 return False
+    #     return True
+        
+    def dfs(self,start,adj,color,prev):
+        color[start]=prev
+        for node in adj[start]:
+            if color[node]==-1:
+                if self.dfs(node,adj,color,1-prev)==False:
                     return False
+            elif color[node]==color[start]:
+                return False
         return True
             
 	def isBipartite(self, V, adj):
 		#code here
 		color=[-1]*V
+		prev=0
 		for i in range(V):
 		    if color[i]==-1:
-		        if self.bfs(i,adj,color)==False:
+		        if self.dfs(i,adj,color,prev)==False:
 		            return False
 	    return True
 
