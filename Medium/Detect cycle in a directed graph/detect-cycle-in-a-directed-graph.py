e9964476680c222aeaac31105e275e1b8907988c
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-04-02 23:37:58 +0530

    Updated Solution - GfG to GitHub

--- Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.py
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.py
@@ -1,27 +1,47 @@
-#User function Template for python3
-
-
+from collections import deque
 class Solution:
-    def check(self,start,adj,vis,pathvis):
-        vis[start]=1
-        pathvis[start]=1
-        for  node in adj[start]:
-            if vis[node]==0:
-                if self.check(node,adj,vis,pathvis)==True:
-                    return True
-            elif pathvis[node]==1:
-                return True
-        pathvis[start]=0
-        return False
-    #Function to detect cycle in a directed graph.
+    # def check(self,start,adj,vis,pathvis):
+    #     vis[start]=1
+    #     pathvis[start]=1
+    #     for  node in adj[start]:
+    #         if vis[node]==0:
+    #             if self.check(node,adj,vis,pathvis)==True:
+    #                 return True
+    #         elif pathvis[node]==1:
+    #             return True
+    #     pathvis[start]=0
+    #     return False
     def isCyclic(self, V, adj):
-        # code here
-        vis=[0]*V
-        pathvis=[0]*V
+        # vis=[0]*V
+        # pathvis=[0]*V
+        # for i in range(V):
+        #     if vis[i]==0:
+        #         if self.check(i,adj,vis,pathvis)==True:
+        #             return True
+        # return False
+        
+        
+        #using kahn's bfs algorithm
+        
+        indegree=[0]*V
         for i in range(V):
-            if vis[i]==0:
-                if self.check(i,adj,vis,pathvis)==True:
-                    return True
+            for node in adj[i]:
+                indegree[node]+=1
+        q=deque()
+        for i in range(V):
+            if indegree[i]==0:
+                q.append(i)
+        topo=0
+        while q:
+            node=q.popleft()
+            topo+=1
+            for neighbor in adj[node]:
+                indegree[neighbor]-=1
+                if indegree[neighbor]==0:
+                    q.append(neighbor)
+        # print(topo)
+        if topo!=V:
+            return True
         return False
 
 
