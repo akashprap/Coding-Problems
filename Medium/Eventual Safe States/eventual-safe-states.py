9225808436fc41fdcdb227dfe7fc71e7be9c250a
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-04-04 13:33:22 +0530

    Updated Solution - GfG to GitHub

--- Medium/Eventual Safe States/eventual-safe-states.py
+++ Medium/Eventual Safe States/eventual-safe-states.py
@@ -1,37 +1,59 @@
 #User function Template for python3
-
+from collections import defaultdict,deque
 from typing import List
 from sys import setrecursionlimit
 setrecursionlimit(10**6)
 class Solution:
-    def dfs(self,start,adj,vis,pathvis,check):
-        vis[start]=1
-        pathvis[start]=1
-        check[start]=0
-        for node in adj[start]:
-            if vis[node]==0:
-                if self.dfs(node,adj,vis,pathvis,check)==True:
-                    check[node]=0
-                    return True
-            elif pathvis[node]==1:
-                check[node]=0
-                return True
-        check[start]=1
-        pathvis[start]=0
-        return False
-    def eventualSafeNodes(self, n: int, adj : List[List[int]]) -> List[int]:
+    # def dfs(self,start,adj,vis,pathvis,check):
+    #     vis[start]=1
+    #     pathvis[start]=1
+    #     check[start]=0
+    #     for node in adj[start]:
+    #         if vis[node]==0:
+    #             if self.dfs(node,adj,vis,pathvis,check)==True:
+    #                 check[node]=0
+    #                 return True
+    #         elif pathvis[node]==1:
+    #             check[node]=0
+    #             return True
+    #     check[start]=1
+    #     pathvis[start]=0
+    #     return False
+    def eventualSafeNodes(self, V: int, adj : List[List[int]]) -> List[int]:
         # code here
-        vis=[0]*n
-        pathvis=[0]*n
-        check=[0]*n
+        # vis=[0]*n
+        # pathvis=[0]*n
+        # check=[0]*n
+        # ans=[]
+        # for i in range(n):
+        #     if not vis[i]:
+        #         self.dfs(i,adj,vis,pathvis,check)
+        # for i in range(n):
+        #     if check[i]:
+        #         ans.append(i)
+        # return ans
+        
+        
+        # swapping nodes direction
+        adjrev=defaultdict(list)
+        indegree=[0]*V
+        for i in range(V):
+            for nodes in adj[i]:
+                adjrev[nodes].append(i)
+                indegree[i]+=1
+        q=deque()
+        for i in range(V):
+            if indegree[i]==0:
+                q.append(i)
         ans=[]
-        for i in range(n):
-            if not vis[i]:
-                self.dfs(i,adj,vis,pathvis,check)
-        for i in range(n):
-            if check[i]:
-                ans.append(i)
-        return ans
+        while q:
+            node=q.popleft()
+            ans.append(node)
+            for neighbor in adjrev[node]:
+                indegree[neighbor]-=1
+                if indegree[neighbor]==0:
+                    q.append(neighbor)
+        return sorted(ans)
 
 
 
