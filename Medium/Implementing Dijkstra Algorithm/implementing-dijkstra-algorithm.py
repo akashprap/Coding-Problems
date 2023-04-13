b5dcb032c49c3c58c573d686d4df0bc7a4946554
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-04-14 00:19:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.py
@@ -0,0 +1,46 @@
+import heapq
+class Solution:
+
+    #Function to find the shortest distance of all the vertices
+    #from the source vertex S.
+    def dijkstra(self, V, adj, S):
+        #code here
+        dist=[float("inf") for i in range(V)]
+        dist[S]=0
+        pq=[(0,S)]
+        while pq:
+            dis,node=heapq.heappop(pq)
+            for nei in adj[node]:
+                if dis+nei[1]<dist[nei[0]]:
+                    dist[nei[0]]=dis+nei[1]
+                    heapq.heappush(pq,(dist[nei[0]],nei[0]))
+        return dist
+            
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases):
+        V,E = map(int,input().strip().split())
+        adj = [[] for i in range(V)]
+        for i in range(E):
+            u,v,w = map(int,input().strip().split())
+            adj[u].append([v,w])
+            adj[v].append([u,w])
+        S=int(input())
+        ob = Solution()
+        
+        res = ob.dijkstra(V,adj,S)
+        for i in res:
+            print(i,end=" ")
+        print()
+# } Driver Code Ends
