280d7d412c04412c4176703e909323293ba7fe4c
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-04-15 09:03:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Find Total Time Taken/find-total-time-taken.py
@@ -0,0 +1,52 @@
+from typing import List
+
+
+class Solution:
+    def totalTime(self, n : int, arr : List[int], time : List[int]) -> int:
+        t=0
+        line=[0]*n
+        for i in range(0,n):
+            if(t >= line[arr[i]-1]):
+                t+=1
+                line[arr[i]-1] = t + time[arr[i]-1]
+            else:
+                t = line[arr[i]-1]
+                line[arr[i]-1] = t + time[arr[i]-1]
+                
+        return t-1 
+
+
+
+#{ 
+ # Driver Code Starts
+class IntArray:
+    def __init__(self) -> None:
+        pass
+    def Input(self,n):
+        arr=[int(i) for i in input().strip().split()]#array input
+        return arr
+    def Print(self,arr):
+        for i in arr:
+            print(i,end=" ")
+        print()
+
+
+if __name__=="__main__":
+    t = int(input())
+    for _ in range(t):
+        
+        n = int(input())
+        
+        
+        arr=IntArray().Input(n)
+        
+        
+        time=IntArray().Input(n)
+        
+        obj = Solution()
+        res = obj.totalTime(n, arr, time)
+        
+        print(res)
+        
+
+# } Driver Code Ends
