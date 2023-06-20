5bedf7980ff0cd5ff4aec17e6455100e6f47c4a5
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-06-20 07:15:47 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Matchsticks game/matchsticks-game.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+class Solution:
+    def matchGame(self, N):
+         # code here
+        if N%5==0:
+            return -1
+        return N%5
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.matchGame(N))
+# } Driver Code Ends
