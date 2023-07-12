618c3544843dd6d786d979af3a218aaef74939dd
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-07-12 09:28:56 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Power Of Numbers/power-of-numbers.py
@@ -0,0 +1,34 @@
+#User function Template for python3
+
+class Solution:
+    #Complete this function
+    def power(self,N,R):
+        mod=10**9+7
+        #Your code here
+        return pow(N,R,mod)
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        N=input()
+        R=N[::-1]
+        
+        ob=Solution();
+        ans=ob.power(int(N),int(R))
+        print(ans)
+        
+        T-=1
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
