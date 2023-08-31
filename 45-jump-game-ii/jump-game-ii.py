6ee5e5565bcc7e813f2dd388cb4d07080a04e9be
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-08-31 13:48:57 +0530

    Time: 110 ms (95.18%) | Memory: 17.4 MB (44.70%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,9 +1,13 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        n=len(nums)
-        dp=[1e9]*n
-        dp[0]=0
-        for i in range(n):
-            for j in range(i+1,min(nums[i]+i+1,n)):
-                dp[j]=min(dp[j],1+dp[i])
-        return dp[n-1] if dp[n-1]!=1e9 else -1
+        l=0
+        r=0
+        ans=0
+        farthest=0
+        while r<len(nums)-1:
+            for i in range(l,r+1):
+                farthest=max(farthest,i+nums[i])
+            l=r+1
+            r=farthest
+            ans+=1
+        return ans
