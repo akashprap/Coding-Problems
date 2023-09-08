6730db223812b563f6003c22191d1ae6c6043f26
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-09-08 13:29:34 +0530

    Time: 42 ms (60.08%) | Memory: 16.2 MB (71.54%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,15 +1,14 @@
 class Solution:
-    # def comb(self,n,r):
-    #     return math.factorial(n)//(math.factorial(n-r)*(math.factorial(r)))
-    def generate(self, numRows: int) -> List[List[int]]:
+    def generate(self, n: int) -> List[List[int]]:
         ans=[[1]]
-        for i in range(1,numRows):
-            temp=1
-            zu=[1]
-            for j in range(i):
-                temp=temp*(i-j)
-                temp=temp//(j+1)
-                zu.append(temp)
-            ans.append(zu)
+        for i in range(1,n):
+            temp=[1]
+            for j in range(1,i):
+                temp.append(ans[-1][j-1]+ans[-1][j])
+            temp.append(1)
+            # print(ans)
+            ans.append(temp)
         return ans
 
+
+        
