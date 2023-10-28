a070b989b5d79bcf771e9201e9f1bd0b8f0c2491
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-10-28 23:23:15 +0530

    Time: 418 ms (72.31%) | Memory: 16.4 MB (53.99%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,21 +1,35 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s)==0:
-            return 0
-        max_length = 1
-        start = 0
-        dp = [[False]*len(s) for _ in range(len(s))]
-        for i in range(len(s)):
-            dp[i][i] = True
-            if i < len(s) - 1 and s[i] == s[i+1]:
-                dp[i][i+1] = True
-                start = i
-                max_length = 2
-        for length in range(3, len(s) + 1):
-            for i in range(len(s) - length + 1):
-                j = i + length - 1
-                if dp[i+1][j-1] and s[i] == s[j]:
-                    dp[i][j] = True
-                    start = i
-                    max_length = length
-        return s[start:start + max_length]
+        ans=""
+        maxi=0
+        n=len(s)
+        for ind in range(len(s)):
+            #expand as s[i] as centre of string
+            #this will produce only odd length substring
+            i=ind
+            j=ind
+            while i>=0 and j<n:
+                if s[i]==s[j]:
+                    if (j-i+1)>maxi:
+                        maxi=j-i+1
+                        ans=s[i:j+1]
+                    i-=1
+                    j+=1
+                else:
+                    break
+            #to generate even length
+            i=ind
+            j=ind+1
+            while i>=0 and j<n:
+                if s[i]==s[j]:
+                    if (j-i+1)>maxi:
+                        maxi=j-i+1
+                        ans=s[i:j+1]
+                    i-=1
+                    j+=1
+                else:
+                    break
+        return ans
+
+
+        
