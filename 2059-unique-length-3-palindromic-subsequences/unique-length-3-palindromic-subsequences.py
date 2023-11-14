f6020a8cdccb6a12e765cdf7252f2d848e7a8990
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2023-11-14 14:17:37 +0530

    Time: 146 ms (86.84%) | Memory: 17.3 MB (24.34%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.py
@@ -1,9 +1,8 @@
 class Solution:
     def countPalindromicSubsequence(self, s: str) -> int:
         ans = 0
-        for c in set(s):
-            i = s.find(c)
-            j = s.rfind(c)
-            if i != -1 and j != -1 and i != j:
-                ans += len(set(s[i+1:j]))
+        char_positions = {c: [s.find(c), s.rfind(c)] for c in set(s)}
+        for positions in char_positions.values():
+            if positions[0] != positions[1]:
+                ans += len(set(s[positions[0]+1:positions[1]]))
         return ans
