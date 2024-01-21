475de3d5459d0ab0d703f517dc5c38a4226d555c
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2024-01-21 11:58:41 +0530

    Time: 37 ms (66.98%) | Memory: 16.5 MB (60.22%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,4 +1,5 @@
 class Solution:
+    # memozation
     def f(self,ind,nums,dp):
         if ind>=len(nums):
             return 0
@@ -9,6 +10,16 @@ class Solution:
         dp[ind]= max(take,nottake)
         return dp[ind]
     def rob(self, nums: List[int]) -> int:
-        dp={}
-        return self.f(0,nums,dp)
+        #tabulation
+        # return self.f(0,nums,dp)
+        n=len(nums)
+        dp=[nums[0]]*(n)
+        for ind in range(1,n):
+            nottake=dp[ind-1]
+            if ind-2>=0:
+                take=nums[ind]+dp[ind-2]
+            else:
+                take=nums[ind]
+            dp[ind]=max(nottake,take)
+        return dp[n-1]
         
