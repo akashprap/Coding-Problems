888df456273ae29ba72e550baf75702709d4d09f
Akash Prajapati <76718079+akashprap@users.noreply.github.com>
2024-03-14 21:42:27 +0530

    Time: 229 ms (30.71%) | Memory: 20.5 MB (17.85%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -3,8 +3,6 @@ class Solution:
         dct=defaultdict(int)
         dct[0]=1
         ans=0
-        i=0
-        j=0
         curr=0
         for j in range(len(nums)):
             curr+=nums[j]
